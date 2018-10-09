10e90aa8c66079ed4f3034bc22c2375310ba6149
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2018-10-09 15:30:59 +0800

    增加汉诺塔算法

---
+++ 北京大学/hanoi.js
@@ -0,0 +1,29 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin
+});
+
+const init = str => Math.min(10, Math.max(1, str | 0));
+
+const hanoi = (n, src, mid, dest) => {
+  if (n === 1) {
+    console.log("%s -> %s", src, dest);
+    return;
+  }
+  hanoi(n - 1, src, dest, mid);
+  console.log("%s -> %s", src, dest);
+  hanoi(n - 1, mid, src, dest);
+};
+
+// A B C 代表三个柱子，要求从 A -> C
+const calc = str => {
+  const n = init(str);
+  hanoi(n, "A", "B", "C");
+};
+
+// 汉诺塔问题, 给出具体的走法
+// 输入：盘子的数量 n 满足 (1 <= n <= 10)
+rl.on("line", input => {
+  console.log(calc(input.trim()));
+});
