d5a23fa0ab047ada4545732012804f370477ef56
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2018-10-15 19:20:08 +0800

    bak

--- sudoku.js
+++ sudoku.js
@@ -6,6 +6,23 @@ const rl = readline.createInterface({
 
 const init = str => str.split(" ").map(x => x | 0);
 
+const output = (list, isShadow = false, key = "options") => {
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(row => {
+    const start = row * 9;
+    const end = start + 9;
+    if (isShadow) {
+      console.log(
+        list
+          .slice(start, end)
+          .map(x => Array.from(x[key]).join("."))
+          .join("\t")
+      );
+    } else {
+      console.log(list.slice(start, end).join("\t"));
+    }
+  });
+};
+
 // 计算当前格子索引的同行同列同宮的其他格子索引值
 const getOthers = (() => {
   const dict = {};
@@ -65,7 +82,21 @@ const setOptions = (index, list, shadow) => {
   // 获取同行，同列，同宫的其他索引值，挨个删除当前值
   getOthers(index).forEach(i => {
     const { options, without } = shadow[i];
-    if (options.size === 1) return; // 无须删除
+    if (options.size === 1) {
+      if (!options.has(value)) return;
+      if (value === 2 && (i === 34 || i === 30)) {
+        console.log(
+          "index: %d; i: %d; value: %d; options: %s, options.has(value): %s",
+          index,
+          i,
+          value,
+          Array.from(options),
+          options.has(value)
+        );
+      }
+      // 照理来说要删除，如果已经只有一个了，且和当前值相等，说明出错了，抛出异常
+      throw Error("冲突了");
+    }
     options.delete(value);
     without.add(value); // 其他的格子排除项里加该值
     // 如果剔除后长度为 1, 即确定了答案，则递归设置
@@ -96,7 +127,17 @@ const setRequired = (i, list, shadow) => {
   }
 };
 
-const startup = (list, shadow) => {
+const tryStack = []; // 尝试的栈
+
+const startup = list => {
+  const shadow = [];
+  for (let i = 0; i < 81; i += 1) {
+    shadow[i] = {
+      // 利用 without 可以缩小 options, 如果某个 option 在这里不存在，那么该 option 就是当前格子的正解
+      without: new Set([]), // 一定不出现的
+      options: new Set(list[i] ? [list[i]] : [1, 2, 3, 4, 5, 6, 7, 8, 9]) // 可选择
+    };
+  }
   list.forEach((x, i) => {
     // 设置可能的选项
     setOptions(i, list, shadow);
@@ -109,30 +150,52 @@ const startup = (list, shadow) => {
     setRequired(i, list, shadow);
   });
 
-  return list;
+  output(list);
+  output(shadow, true);
+
+  // 检查是否还有零值
+  if (list.find(x => !x) === undefined) return list;
+
+  // 尝试栈为空则找到选择最少的不确定项压栈
+  if (tryStack.length === 0) {
+    const unknow = [];
+    for (let index = 0; index < 81; index += 1) {
+      if (list[index] === 0) {
+        unknow.push({ index, options: shadow[index].options });
+      }
+    }
+    unknow.sort((a, b) => (a.options.size > b.options.size ? 1 : -1));
+    unknow[0].options.forEach(x => {
+      const fork = [].concat(list);
+      fork[unknow[0].index] = x;
+      tryStack.push(fork);
+    });
+  }
+  const fork = tryStack.pop();
+
+  // output(list);
+  output(list);
+  output(shadow, true);
+  // process.exit();
+  try {
+    return startup(fork);
+  } catch (e) {
+    return startup(list);
+  }
 };
 
 const calc = list => {
-  const shadow = [];
-  for (let i = 0; i < 81; i += 1) {
-    shadow[i] = {
-      // 利用 without 可以缩小 options, 如果某个 option 在这里不存在，那么该 option 就是当前格子的正解
-      without: new Set([]), // 一定不出现的
-      options: new Set(list[i] ? [list[i]] : [1, 2, 3, 4, 5, 6, 7, 8, 9]) // 可选择
-    };
+  try {
+    const resolve = startup(list);
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(row => {
+      const start = row * 9;
+      const end = start + 9;
+      return resolve.slice(start, end);
+    });
+  } catch (e) {
+    console.error("哈哈", e);
+    return [];
   }
-  startup(list, shadow);
-  return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(row => {
-    const start = row * 9;
-    const end = start + 9;
-    console.log(
-      shadow
-        .slice(start, end)
-        .map(x => Array.from(x.options).join("."))
-        .join("\t\t")
-    );
-    return list.slice(start, end);
-  });
 };
 
 // 整理输入信息, 把矩阵整理成一维数组，长度 81, 便于后续单次循环
