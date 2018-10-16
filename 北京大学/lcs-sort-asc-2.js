98db16cf8256e9786e2890a0d4f84532ed61bc3b
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2018-10-16 14:47:47 +0800

    增加另外一种思路的最长上升子序列的算法

--- 北京大学/lcs-sort-asc-2.js
+++ 北京大学/lcs-sort-asc-2.js
@@ -6,18 +6,18 @@ const rl = readline.createInterface({
 
 const init = str => str.split(" ").map(x => x | 0);
 
-// “人人为我” 的更新思路
-// 发现前面有比我小的值，那么我就 +1
+// “我为人人” 的更新思路
+// 发现后面有比我大的值，后面的就 +1
 const lcs = list => {
   const { length } = list;
   const maxLen = []; // 记录每个位置对应的最大上升子串的长度, 初始值均为 1, 只有本身
   maxLen.length = length;
   maxLen.fill(1);
 
-  for (let i = 1; i < length; i += 1) {
-    for (let j = 0; j < i; j += 1) {
-      if (list[i] > list[j]) {
-        maxLen[i] = Math.max(maxLen[i], maxLen[j] + 1);
+  for (let i = 0; i < length; i += 1) {
+    for (let j = i + 1; j < length; j += 1) {
+      if (list[j] > list[i]) {
+        maxLen[j] = Math.max(maxLen[j], maxLen[i] + 1);
       }
     }
   }
