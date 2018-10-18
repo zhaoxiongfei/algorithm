f75fe5d1494b911cbb10013449e00261db9ab469
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2018-10-18 14:27:07 +0800

    增加罗马数字到int的转换

--- leetcode/int2roman.js
+++ leetcode/int2roman.js
@@ -30,6 +30,81 @@ const intToRoman = num => {
   return value;
 };
 
+const dict2 = {
+  I: 1,
+  IV: 4,
+  V: 5,
+  IX: 9,
+  X: 10,
+  XL: 40,
+  L: 50,
+  XC: 90,
+  C: 100,
+  CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000
+};
+
+const romanToInt = str => {
+  const { length } = str;
+  let num = 0;
+  let i = 0;
+  while (i < length) {
+    const curr = str[i];
+    const post = str[i + 1];
+    const v = dict2[`${curr}${post}`];
+    if (v) {
+      num += v;
+      i += 2;
+    } else {
+      num += dict2[curr];
+      i += 1;
+    }
+  }
+
+  return num;
+};
+
+// 改良后的，从后往前查
+const romanToInt2 = str => {
+  const { length } = str;
+  let num = 0;
+  let i = length;
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+  while (i) {
+    i -= 1;
+    const curr = map[str[i]];
+    const pre = map[str[i - 1]];
+
+    num += curr;
+    if (pre < curr) {
+      num -= pre;
+      i -= 1;
+    }
+  }
+
+  return num;
+};
+
 console.log(intToRoman(1994));
 console.log(intToRoman(3));
 console.log(intToRoman(126543));
+
+console.log(romanToInt(intToRoman(1994)));
+console.log(romanToInt(intToRoman(3)));
+console.log(romanToInt(intToRoman(126543)));
+console.log(romanToInt("MCDLXXVI"));
+
+console.log(romanToInt2(intToRoman(1994)));
+console.log(romanToInt2(intToRoman(3)));
+console.log(romanToInt2(intToRoman(126543)));
+console.log(romanToInt2("MCDLXXVI"));
