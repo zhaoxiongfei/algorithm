0a21b9d6416e981808b4986ab6bc5b93c16b4eea
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2018-10-23 09:49:46 +0800

    N 皇后放置的问题，一个用栈，一个用递归实现

---
+++ leetcode/n-queens-2.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+const solveNQueens = n => {
+  let solutions = 0;
+  const queens = [];
+
+  const search = () => {
+    if (queens.length === n) {
+      solutions += 1;
+    } else {
+      // 逐个判断，寻找可能的下一个皇后的位置
+      for (let i = 0; i < n; i += 1) {
+        let next = true;
+        for (let j = 0; j < queens.length; j += 1) {
+          if (
+            queens[j] === i ||
+            queens[j] - j === i - queens.length ||
+            queens[j] + j === i + queens.length
+          ) {
+            next = false;
+            break;
+          }
+        }
+        if (next) {
+          queens.push(i);
+          search();
+          queens.pop();
+        }
+      }
+    }
+  };
+
+  search();
+  return solutions;
+};
+
+console.log(solveNQueens(11));
---
+++ leetcode/n-queens.js
@@ -0,0 +1,59 @@
+// https://leetcode-cn.com/problems/n-queens/description/
+// n 的皇后放入 n * n 个格子中，每行，每列有且仅有一个，每个皇后的两条对角线也不能存在其他皇后
+
+const format = solutions => {
+  if (solutions.length === 0) return solutions;
+  const op = ".".repeat(solutions[0].length).split("");
+  return solutions.map(solution =>
+    solution.map(col => {
+      const line = op.slice();
+      line[col] = "Q";
+      return line.join("");
+    })
+  );
+};
+
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+const solveNQueens = n => {
+  const solutions = [];
+  const tryStack = [];
+
+  const init = [];
+  for (let i = 0; i < n; i += 1) init.push(i);
+
+  // 依次将第一个皇后的可选情况压入待尝试的栈
+  for (let i = 0; i < n; i += 1) tryStack.push([i]);
+  // 逐个尝试栈里的选项，尝试的过程中可能会有新的情况继续压入栈内
+  while (tryStack.length) {
+    const solution = tryStack.shift();
+    const { length } = solution;
+    if (length === n) {
+      // 满足条件压入结果
+      solutions.push(solution);
+      continue;
+    }
+    // 下一个皇后可能的列位置序号
+    const options = new Set(init);
+    // 依次计算每一个已压入的选项，操作后续的可选项目
+    for (let i = 0; i < length; i += 1) {
+      const col = solution[i];
+      options.delete(col); // 删除同列的
+      options.delete(col + length - i); // 删除主对角线的
+      options.delete(col - length + i); // 删除次对角线的
+    }
+    for (const option of options) {
+      // 这个只能选择这个选项，压入等待后续继续监测
+      const newSolution = solution.slice();
+      newSolution.push(option);
+      // 不满足条件压入待尝试栈
+      tryStack.push(newSolution);
+    }
+  }
+
+  return solutions.length;
+};
+
+console.log(solveNQueens(11));
