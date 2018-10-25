0155392bd3ca83d37edbf498b77caddcfb3ffdb3
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2018-10-25 11:50:46 +0800

    完成一个搜索旋转列表的功能

---
+++ leetcode/search-in-rotated-sorted-array.js
@@ -0,0 +1,61 @@
+// https://leetcode-cn.com/problems/search-in-rotated-sorted-array/description/
+
+const binarySearch = (nums, target, start, end) => {
+  let left = start;
+  let right = end;
+  while (left < right) {
+    if (target === nums[left]) return left;
+    if (target === nums[right]) return right;
+    if (left + 1 === right) return -1;
+    if (target < nums[left]) return -1;
+    if (target > nums[right]) return -1;
+
+    const middle = Math.ceil((left + right) / 2);
+    if (target === nums[middle]) return middle;
+    if (target < nums[middle]) {
+      right = middle;
+    } else {
+      left = middle;
+    }
+  }
+
+  return -1;
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// 思路: 对半切开，肯定至少有一半是顺序良好的，顺序良好的放到二分法里查找
+// 另外的一般继续切开
+const search = (nums, target, start = 0, end = nums.length - 1) => {
+  if (start === end) {
+    if (target === nums[start]) return start;
+  }
+  if (start > end) return -1;
+  if (nums[start] === target) return start;
+  if (nums[end] === target) return end;
+  if (nums[start] === nums[end]) return -1;
+
+  const middle = Math.floor((start + end) / 2);
+  // 右半边
+  if (nums[middle] <= nums[end]) {
+    if (target < nums[end] && target >= nums[middle]) {
+      return binarySearch(nums, target, middle, end);
+    }
+    return search(nums, target, start, middle - 1);
+  }
+
+  // 左半边
+  if (target > nums[start] && target <= nums[middle]) {
+    return binarySearch(nums, target, start, middle);
+  }
+  return search(nums, target, middle + 1, end);
+};
+
+console.log(search([8, 1, 2, 3, 4, 5, 6, 7], 6));
+console.log(search([1, 3], 2));
+console.log(search([3, 1], 0));
+console.log(search([4, 5, 6, 7, 0, 1, 2], 1));
+console.log(search([4, 5, 6, 7, 0, 1, 2], 6));
