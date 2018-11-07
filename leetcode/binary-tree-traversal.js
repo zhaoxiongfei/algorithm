5c6fa43fb037a925783b3cb452d5a031cccac8e4
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2018-11-07 16:28:21 +0800

    增加二叉树中序遍历的高性能方法

--- leetcode/binary-tree-traversal.js
+++ leetcode/binary-tree-traversal.js
@@ -15,6 +15,7 @@ const visitLongestLeftSubTree = (vertex, fn, stack) => {
   }
 };
 
+// 先序遍历
 const traversal2 = root => {
   const stack = [root];
   const list = [];
@@ -27,8 +28,33 @@ const traversal2 = root => {
   return list;
 };
 
+const goAlongLeftBranch = (vertex, stack) => {
+  let node = vertex;
+  while (node) {
+    stack.push(node);
+    node = node.left;
+  }
+};
+
+// 中序遍历
+const traversal3 = root => {
+  const stack = [];
+  const list = [];
+  const fn = list.push.bind(list);
+  let node = root;
+  while (node || stack.length) {
+    goAlongLeftBranch(node, stack);
+    const vertex = stack.pop();
+    fn(vertex.val);
+    node = vertex.right;
+  }
+
+  return list;
+};
+
 const list = [];
 traversal(TreeNode.create([1, 2, 3, 4, 5, 6, 7]), list.push.bind(list));
 console.log(list);
 
 console.log(traversal2(TreeNode.create([1, 2, 3, 4, 5, 6, 7])));
+console.log(traversal3(TreeNode.create([1, 2, 3, 4, 5, 6, 7])));
