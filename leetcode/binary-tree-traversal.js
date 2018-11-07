75e2fbf944f7ec244f9ccfddf67c2cfb82618d88
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2018-11-07 15:52:22 +0800

    增加一种遍历方式

--- leetcode/binary-tree-traversal.js
+++ leetcode/binary-tree-traversal.js
@@ -6,7 +6,29 @@ const traversal = (root, fn) => {
   fn(root.val);
 };
 
+const visitLongestLeftSubTree = (vertex, fn, stack) => {
+  let node = vertex;
+  while (node) {
+    fn(node.val);
+    if (node.right) stack.push(node.right);
+    node = node.left;
+  }
+};
+
+const traversal2 = root => {
+  const stack = [root];
+  const list = [];
+  const fn = list.push.bind(list);
+  while (stack.length) {
+    const vertex = stack.pop();
+    visitLongestLeftSubTree(vertex, fn, stack);
+  }
+
+  return list;
+};
+
 const list = [];
 traversal(TreeNode.create([1, 2, 3, 4, 5, 6, 7]), list.push.bind(list));
-
 console.log(list);
+
+console.log(traversal2(TreeNode.create([1, 2, 3, 4, 5, 6, 7])));
