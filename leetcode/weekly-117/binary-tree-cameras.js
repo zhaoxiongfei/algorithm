b22ead197af38cafb3fbfae833094a118117de93
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2019-01-22 11:55:22 +0800

    参考官方的解答重新实现代码

--- leetcode/weekly-117/binary-tree-cameras.js
+++ leetcode/weekly-117/binary-tree-cameras.js
@@ -7,58 +7,30 @@
  */
 const TreeNode = require("../binary-tree");
 
-const dfs = (root, dp) => {
+// https://leetcode-cn.com/articles/binary-tree-cameras/
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const minCameraCover = root => {
   if (!root) return 0;
-  const calced = dp.get(root);
-  if (calced !== undefined) return calced;
+  const solve = node => {
+    if (!node) return [0, 0, Infinity];
+    const L = solve(node.left);
+    const R = solve(node.right);
+    const mL12 = Math.min(L[1], L[2]);
+    const mR12 = Math.min(R[1], R[2]);
 
-  let count0 = 1; // 根节点有
-  let count1 = 1; // 左子树有
-  let count2 = 1; // 右子树有
+    return [
+      L[1] + R[1],
+      Math.min(L[2] + mR12, R[2] + mL12),
+      1 + Math.min(L[0], mL12) + Math.min(R[0], mR12)
+    ];
+  };
 
-  if (root.left) {
-    count0 += Math.min(
-      dfs(root.left, dp),
-      dfs(root.left.left, dp) + dfs(root.left.right, dp)
-    );
-    if (root.left.left) {
-      count1 += Math.min(
-        dfs(root.left.left, dp),
-        dfs(root.left.left.left, dp) + dfs(root.left.left.right, dp)
-      );
-    }
-    if (root.left.right) {
-      count1 += Math.min(
-        dfs(root.left.right, dp),
-        dfs(root.left.right.left, dp) + dfs(root.left.right.right, dp)
-      );
-    }
-    count2 += dfs(root.left, dp);
-  }
+  const ans = solve(root);
 
-  if (root.right) {
-    count0 += Math.min(
-      dfs(root.right, dp),
-      dfs(root.right.left, dp) + dfs(root.right.right, dp)
-    );
-    if (root.right.left) {
-      count2 += Math.min(
-        dfs(root.right.left, dp),
-        dfs(root.right.left.left, dp) + dfs(root.right.left.right, dp)
-      );
-    }
-    if (root.right.right) {
-      count2 += Math.min(
-        dfs(root.right.right, dp),
-        dfs(root.right.right.left, dp) + dfs(root.right.right.right, dp)
-      );
-    }
-    count1 += dfs(root.right, dp);
-  }
-  const count = Math.min(count0, count1, count2);
-  dp.set(root, count);
-
-  return count;
+  return Math.min(ans[1], ans[2]);
 };
 
 /**
@@ -67,47 +39,37 @@ const dfs = (root, dp) => {
  */
 const minCameraCover2 = root => {
   if (!root) return 0;
-
-  return Math.max(1, dfs(root, new Map()));
-};
-
-const minCameraCover = root => {
-  let res = 0;
-  // 返回值0代表当前不放相机且没有被监控 返回值1代表当前没有放相机但是已经被监控 返回值2代表当前放了相机
-  const _dfs = (node, isRoot) => {
-    // 如果为null，则可用2表示
-    if (node === null) return 1;
-
-    const left = _dfs(node.left);
-    const right = _dfs(node.right);
-
-    if (left === 0 || right === 0) {
-      // 如果子节点有未放置相机且未被监控，则当前节点一定需要放置相机
-      res += 1;
-      return 2;
+  const set = new Set();
+  let ans = 0;
+  const dfs = (node, parent) => {
+    if (!node) return;
+    dfs(node.left, node);
+    dfs(node.right, node);
+    if (
+      (!parent && !set.has(node)) ||
+      !set.has(node.left) ||
+      !set.has(node.right)
+    ) {
+      ans += 1;
+      set.add(parent);
+      set.add(node);
+      set.add(node.left);
+      set.add(node.right);
     }
-    if (left === 1 && right === 1) {
-      // 子节点都已被监控但都未放置相机，则当前可不用放置相机，等待被监控，返回0
-      if (isRoot === true) res += 1; // 如果是根节点，则必须放置一个相机
-      return 0;
-    }
-    if (left === 2 || right === 2) {
-      // 如果子节点已经放置了相机，且排除了子节点未被监控的情况，则当前节点可不放相机，但已被监控，返回1
-      return 1;
-    }
-    return 0;
   };
 
-  _dfs(root, true);
-  return res;
+  set.add(null);
+  dfs(root, null);
+
+  return ans;
 };
 
 console.log(
-  minCameraCover2(TreeNode.create([0, 0, null, 0, null, 0, null, null, 0]))
+  minCameraCover(TreeNode.create([0, 0, null, 0, null, 0, null, null, 0]))
 );
 console.log(
   // minCameraCover(TreeNode.create([0, 0, null, 0, null, 0, null, null, 0]))
-  minCameraCover(TreeNode.create([0, 0, null, 0, null, 0, null, null, 0]))
+  minCameraCover2(TreeNode.create([0, 0, null, 0, null, 0, null, null, 0]))
   // minCameraCover(TreeNode.create([0, 0, 0, null, 0, 0, null, null, 0]))
   // minCameraCover(TreeNode.create([0, 0, null, null, 0, 0, null, null, 0, 0]))
   // minCameraCover(TreeNode.create([0, 0, null, 0, 0]))
